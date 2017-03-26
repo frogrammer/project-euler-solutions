d5a2c164d348d058cf4b3fd1c533eb9862cef33b
Tony Song <tony99019@gmail.com>
2017-03-26 02:09:22 -0500

    Implement simple problem 4 solution

--- src/project_euler_solutions/core.clj
+++ src/project_euler_solutions/core.clj
@@ -1,7 +1,17 @@
 (ns project-euler-solutions.core
   (:require [clojure.math.numeric-tower :as math]))
 
-(defn -main [& args]
-  (println "Hey"))
-
 ;; Problem 4 - Largest palindrome product
+(defn is-palindrome? [num]
+  (= (str num) (clojure.string/reverse (str num))))
+
+(defn largest-palindrome [digit]
+  (let [min-product (math/expt 10 (dec digit))
+        max-product (dec (* min-product 10))
+        all-elements (range min-product (inc max-product))]
+    (apply max
+           (filter is-palindrome?
+                   (for [x1 all-elements x2 all-elements] (* x1 x2))))))
+
+(defn -main [& args]
+  (println (largest-palindrome 3)))
