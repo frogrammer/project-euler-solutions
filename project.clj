49a7e24cef1334b581a5d47141a467d65558519e
Tony Song <tony99019@gmail.com>
2017-03-26 05:19:30 -0500

    Switch to clojure version 1.9.0-alpha15 for spec

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://projecteuler.net/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
   :main project-euler-solutions.core)
