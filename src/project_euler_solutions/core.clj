40682684aeefe160c8ceed353078bdf5a0496c3a
Tony Song <tony99019@gmail.com>
2017-03-26 05:21:54 -0500

    Add function spec for 'largest-palindrome'

--- src/project_euler_solutions/core.clj
+++ src/project_euler_solutions/core.clj
@@ -1,5 +1,7 @@
 (ns project-euler-solutions.core
-  (:require [clojure.math.numeric-tower :as math]))
+  (:require [clojure.math.numeric-tower :as math]
+            [clojure.spec :as s]
+            [clojure.spec.test :as stest]))
 
 ;; Problem 4 - Largest palindrome product
 (defn is-palindrome? [num]
@@ -12,6 +14,11 @@
     (apply max
            (filter is-palindrome?
                    (for [x1 all-elements x2 all-elements] (* x1 x2))))))
+
+(s/fdef project-euler-solutions.core/largest-palindrome
+  :args (s/and (s/cat :arg int?) #(pos? (:arg %)))
+  :ret int?)
+(stest/instrument `largest-palindrome)
 
 (defn -main [& args]
   (println (largest-palindrome 3)))
