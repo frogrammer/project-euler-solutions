6b384b2986821d993c7b63cdf36c202c6724a54d
Tony Song <tony99019@gmail.com>
2017-03-26 17:25:55 -0500

    Implement problem 7 solution

--- src/project_euler_solutions/core.clj
+++ src/project_euler_solutions/core.clj
@@ -3,6 +3,7 @@
             [clojure.spec :as s]
             [clojure.spec.test :as stest]))
 
+
 ;; Problem 4 - Largest palindrome product
 (defn is-palindrome? [num]
   (= (str num) (clojure.string/reverse (str num))))
@@ -15,10 +16,21 @@
            (filter is-palindrome?
                    (for [x1 all-elements x2 all-elements] (* x1 x2))))))
 
+;; Problem 7 - 10001st prime
+(defn is-prime? [num]
+  (reduce (fn [bool current] (and bool (not= 0 (mod num current))))
+          true
+          (range 2 (inc (math/floor (math/sqrt num))))))
+
+(defn find-prime [i-th]
+  (nth (filter is-prime? (drop 2 (range))) (dec i-th)))
+
+
 (s/fdef project-euler-solutions.core/largest-palindrome
   :args (s/and (s/cat :arg int?) #(pos? (:arg %)))
   :ret int?)
 (stest/instrument `largest-palindrome)
 
+
 (defn -main [& args]
-  (println (largest-palindrome 3)))
+  (println (find-prime 10001)))
